05346e0af0ca8fa743e7978d3748874d0f41b620
CisMous <cismous@gmail.com>
2016-05-18 19:51:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# cordova-plugin-deploy
+
+This plugin provides the ability to download and setup application package files on a android device.
+
+## Installation
+
+    cordova plugin add https://github.com/cismous/cordova-plugin-deploy
+
+## Supported Platforms
+
+- Android
---
+++ www/deploy.js
@@ -0,0 +1,10 @@
+var Deploy = {
+  download: function (url, success, failure) {
+    cordova.exec(success, failure, 'Deploy', 'download', [url]);
+  },
+  setup: function (url, success, failure) {
+    cordova.exec(success, failure, 'Deploy', 'setup', [url]);
+  }
+};
+
+module.exports = Deploy;
